cf40eb4ddce1eb39460f14ff73bbdb533eb68792
PunnWind <6621061006@cdti.ac.th>
2025-07-01 11:05:42 +0700

    Update

---
+++ form_user.php
@@ -0,0 +1,8 @@
+<form action="insert_iot.php" method="post">
+    ID: <input type="text" name="ID" required><br>
+    Name: <input type="text" name="Name" required><br>
+    Email: <input type="text" name="Email" required><br>
+    Username: <input type="text" name="UserName" required><br>
+    Password: <input type="password" name="Password" required><br> 
+    <input type="submit" value="Submit">
+    <input type="reset" value="Reset">
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <h2>Welcome to the IoT Data Page</h2>
+    <p class="content">ยินดีต้อนรับสู่หน้าข้อมูล IoT</p>
+    <div id="aaa">
+        โดยปัณณวิชญ์
+    </div>
+
+    <a href="#" class="btn">Register?</a>
+    <a href="#" class="success">Sign in</a>    
+</body>
+</html>
